d89dac48a6d2d0c48d6ee7d6a6283582e6b10235
Jaime Terreu <jaime.terreu@cultureamp.com>
2018-10-14 11:59:53 +1100

    added elm compatibility

---
+++ app/script.js
@@ -0,0 +1,7 @@
+import "../templates/css/reset.css";
+import "./style.css";
+import { Elm } from "./Main.elm";
+
+var mountNode = document.getElementById("elmApp");
+
+Elm.Main.init({ node: mountNode });
